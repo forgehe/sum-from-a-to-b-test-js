04dbd7460a651468d1d6ccab518ad633f900d512
Richard Liao <liao098@gmail.com>
2020-02-07 21:16:06 +0000

    implemented sum.js with recursion

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-
+// eslint-disable-next-line func-style
 function sum(fromN, toN) {
+  let sumA = 0;
+  console.log(`fromN ${fromN}, toN ${toN}`);
   // Sum all the values from fromN up to toN
+
+
+  const recurse = function (fromN, toN) {
+    sumA += fromN;
+    console.log(`recurse: fromN ${fromN}, toN ${toN}, sumA ${fromN}`);
+    if (fromN === toN) {
+
+      return sumA;
+    } else {
+      return recurse(fromN + 1, toN);
+    }
+  };
+  return recurse(fromN, toN);
 }
+
+console.log(sum(3, 5));
 
 module.exports = sum;
