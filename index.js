f0d880696d4116fdada3207ee43126c205e1a3f5
Richard Liao <liao098@gmail.com>
2020-02-07 21:17:57 +0000

    commented out console.log(s)

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // eslint-disable-next-line func-style
 function sum(fromN, toN) {
   let sumA = 0;
-  console.log(`fromN ${fromN}, toN ${toN}`);
+  // console.log(`fromN ${fromN}, toN ${toN}`);
   // Sum all the values from fromN up to toN
 
 
   const recurse = function (fromN, toN) {
     sumA += fromN;
-    console.log(`recurse: fromN ${fromN}, toN ${toN}, sumA ${fromN}`);
+    // console.log(`recurse: fromN ${fromN}, toN ${toN}, sumA ${fromN}`);
     if (fromN === toN) {
 
       return sumA;
@@ -18,6 +18,6 @@ function sum(fromN, toN) {
   return recurse(fromN, toN);
 }
 
-console.log(sum(3, 5));
+// console.log(sum(3, 5));
 
 module.exports = sum;
